4a0660742c23ef3fbcac7826862bf12a0d55f501
Roger Peppe <roger.peppe@canonical.com>
2014-07-08 07:51:33 +0100

    Fix so it works with gopkg.in-style versions. Print full usage on command line.

--- govers.go
+++ govers.go
@@ -22,20 +22,20 @@ It accepts the following flags:
 If the pattern is not specified with the -m flag, it is derived from
 new-package-path and matches any prefix that is the same in all but
 version.  A version is defined to be an element within a package path
-that matches the regular expression "v[0-9.]+".
+that matches the regular expression "(/|\.)v[0-9.]+".
 
 The govers command will also check (unless the -d flag is given)
 that no (recursive) dependencies would be changed if the same govers
 command was run on them. If they would, govers will fail and do nothing.
 
 For example, say a new version of the mgo package is released.
-The old import path was labix.org/v2/mgo, and we want
-to use the new verson, labix.org/v3. In the root of the
+The old import path was gopkg.in/tomb.v2, and we want
+to use the new verson, gopkg.in/tomb.v3. In the root of the
 source tree we want to change, we run:
 
-	govers labix.org/v3
+	govers gopkg.in/tomb.v3
 
-This will change all labix.org/v? imports to use v3.
+This will change all gopkg.in/tomb.v2 imports to use v3.
 It will also check that all external packages that we're
 using are also using v3, making sure that our program
 is consistently using the same version throughout.
@@ -58,6 +58,49 @@ import (
 	"strings"
 )
 
+const help = `
+The govers command searches all Go packages under the current
+directory for imports with a prefix matching a particular pattern, and
+changes them to another specified prefix. As with gofmt and gofix, there is
+no backup - you are expected to be using a version control system.
+It prints the names of any packages that are modified.
+
+Usage:
+
+	govers [-d] [-m regexp] [-n] new-package-path
+
+It accepts the following flags:
+
+	-d
+		Suppress dependency checking
+	-m regexp
+		Search for and change imports which have the
+		given pattern as a prefix (see below for the default).
+	-n
+		Don't make any changes; just perform checks.
+
+If the pattern is not specified with the -m flag, it is derived from
+new-package-path and matches any prefix that is the same in all but
+version.  A version is defined to be an element within a package path
+that matches the regular expression "(/|\.)v[0-9.]+".
+
+The govers command will also check (unless the -d flag is given)
+that no (recursive) dependencies would be changed if the same govers
+command was run on them. If they would, govers will fail and do nothing.
+
+For example, say a new version of the mgo package is released.
+The old import path was gopkg.in/tomb.v2, and we want
+to use the new verson, gopkg.in/tomb.v3. In the root of the
+source tree we want to change, we run:
+
+	govers gopkg.in/tomb.v3
+
+This will change all gopkg.in/tomb.v2 imports to use v3.
+It will also check that all external packages that we're
+using are also using v3, making sure that our program
+is consistently using the same version throughout.
+`
+
 var (
 	match          = flag.String("m", "", "change imports with a matching prefix")
 	noEdit         = flag.Bool("n", false, "don't make any changes; perform checks only")
@@ -66,8 +109,7 @@ var (
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "usage: govers new-package-prefix\n")
-		flag.PrintDefaults()
+		fmt.Printf("%s", help[1:])
 		os.Exit(2)
 	}
 	flag.Parse()
@@ -196,6 +238,7 @@ func (ctxt *context) checkPackage(path string) {
 	for _, impPath := range allImports {
 		if p := ctxt.fixPath(impPath); p != impPath {
 			if ep == nil {
+				logf("package %q is using inconsistent path %q", path, impPath)
 				ctxt.failed = true
 				continue
 			}
@@ -262,7 +305,7 @@ func (ctxt *context) fixPath(p string) string {
 	return p
 }
 
-const versPat = `/v[0-9.]+`
+const versPat = `[/|\.]v[0-9]+`
 
 // pathVersionPat returns a pattern that will match any
 // package path that's the same except possibly
